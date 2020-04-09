Fixed Cifar10 Reconstruction and Rotation task bug
Signed-off-by: Fabrice <fabrice.normandin@gmail.com>

datasets/subset.py
@@ -39,8 +39,9 @@ class VisionDatasetSubset(TensorDataset):
         assert shape_1 == shape_2, f"Shapes should be the same: {shape_1}, {shape_2}"
         
         # Add a "channel" dimension, if none exists.
-        if self.data.dim() == 3:
-            self.data.unsqueeze_(1)
+        from utils.utils import fix_channels
+        self.data = fix_channels(self.data)
+
         # Convert the samples to float, if not done already. 
         if self.data.dtype == torch.uint8:
             self.data = self.data.float() / 255

models/classifier.py
@@ -3,7 +3,8 @@ from abc import ABC, abstractmethod
 from collections import OrderedDict
 from contextlib import contextmanager
 from dataclasses import dataclass
-from typing import Any, Dict, List, NamedTuple, Optional, Tuple, Type, TypeVar, Union
+from typing import (Any, Dict, List, NamedTuple, Optional, Tuple, Type,
+                    TypeVar, Union)
 
 import torch
 from simple_parsing import MutableField as mutable_field
@@ -18,6 +19,7 @@ from common.losses import LossInfo
 from common.metrics import accuracy, get_metrics
 from config import Config
 from tasks import AuxiliaryTask, AuxiliaryTaskOptions
+from utils.utils import fix_channels
 
 
 class Classifier(nn.Module):
@@ -178,13 +180,3 @@ class Classifier(nn.Module):
         if self.current_task_id is not None:
             classifier = self.task_classifiers[self.current_task_id]
         return classifier(h_x)
-
-
-def fix_channels(x_batch: Tensor) -> Tensor:
-    if x_batch.dim() == 3:
-        return x_batch.unsqueeze(1)
-    else:
-        if x_batch.shape[1] != min(x_batch.shape[1:]):
-            return x_batch.transpose(1, -1)
-        else:
-            return x_batch