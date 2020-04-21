fc6ce617de281c70afd4c8d8b92369809f71f1f8
Fabrice <fabrice.normandin@gmail.com>
2020-04-21 18:14:29 -0400

    Squashed commit of the following:
    commit 9971aee88b9cf547a899e660ec4e7bc1c374ca99
    Author: Fabrice Normandin <normandf@beluga4.int.ets1.calculquebec.ca>
    Date:   Tue Apr 21 18:12:19 2020 -0400
    
        Fixed really dumb sbatch bug!
    
        Signed-off-by: Fabrice Normandin <normandf@beluga4.int.ets1.calculquebec.ca>
    
    commit de5febd4b1f8a0c58bc205f0ad9d08e68ab06805
    Author: Fabrice Normandin <normandf@beluga4.int.ets1.calculquebec.ca>
    Date:   Tue Apr 21 17:42:36 2020 -0400
    
        Fixed number of args bug
    
        Signed-off-by: Fabrice Normandin <normandf@beluga4.int.ets1.calculquebec.ca>
    
    commit 17570e219eb266663241d860ec32243233929abb
    Author: Fabrice <fabrice.normandin@gmail.com>
    Date:   Tue Apr 21 17:36:14 2020 -0400
    
        Debugging sbatch issues
    
        Signed-off-by: Fabrice <fabrice.normandin@gmail.com>
    
    Signed-off-by: Fabrice <fabrice.normandin@gmail.com>

--- scripts/test_job.sh
+++ scripts/test_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=3:00:00                   # The job will run for 3 hours
-#SBATCH --output /scratch/<user>/slurm-%N-%j.out  # Write the log in $SCRATCH
+#SBATCH --output /scratch/normandf/slurm-%N-%j.out  # Write the log in $SCRATCH
 
 module load httpproxy
 module load python/3.7
@@ -22,17 +22,19 @@ cd ~/repos/SSCL
 
 # 2. Copy your dataset on the compute node
 # IMPORTANT: Your dataset must be compressed in one single file (zip, hdf5, ...)!!!
-cp --update $SCRATCH/data.zip $SLURM_TMPDIR
+cp --update $SCRATCH/data.zip -d $SLURM_TMPDIR
 
 # 3. Eventually unzip your dataset
 unzip -n $SLURM_TMPDIR/data.zip -d $SLURM_TMPDIR
 
 # WANDB_MODE=dryrun
 # export WANDB_MODE
+echo "Executing main.py with additional args: ${@:1}"
+
 python -u main.py task-incremental \
 	--data_dir $SLURM_TMPDIR/data \
-	--log_dir_root $SLURM_TMPDIR/SSCL "$@"
-	
+	--log_dir_root $SLURM_TMPDIR/SSCL "${@:1}"
+
 rsync -r --delete $SLURM_TMPDIR/SSCL/* $SCRATCH/SSCL
 wandb sync $SCRATCH/SSCL/wandb/ # Not guaranteed to work given CC's network restrictions.
 # To make sure, run `wandb sync $SCRATCH/SSCL/wandb/` from a login node
