13b045ad81feab3ff51f2f4918e35d36813d8a42
Fabrice <fabrice.normandin@gmail.com>
2020-04-29 18:35:05 -0400

    Updated the fork of Nitarshan's repo to master
    Signed-off-by: Fabrice <fabrice.normandin@gmail.com>

--- tasks/simclr/simclr_task.py
+++ tasks/simclr/simclr_task.py
@@ -13,7 +13,7 @@ from tasks.auxiliary_task import AuxiliaryTask
 try:
     from .falr.config import HParams
     from .falr.data import SimCLRAugment
-    from .falr.experiment import nt_xent_loss
+    from .falr.losses import SimCLRLoss
     from .falr.models import Projector
 except ImportError as e:
     print(f"Couldn't import the modules from the falr submodule: {e}")
@@ -39,10 +39,11 @@ class SimCLRTask(AuxiliaryTask):
         self.augment = Compose([
             ToPILImage(),
             SimCLRAugment(self.options),
-            Lambda(lambda tup: torch.stack([to_tensor(tup[0]), to_tensor(tup[1])]))
+            Lambda(lambda tup: torch.stack([tup[0], tup[1]]))
         ])
         self.projector = Projector(self.options)
         self.i = 0
+        self.loss = SimCLRLoss(self.options.proj_dim)
 
     def get_loss(self, x: Tensor, h_x: Tensor, y_pred: Tensor, y: Tensor=None) -> LossInfo:
         # TODO: is there a more efficient way to do this than with a list comprehension? (torch multiprocessing-ish?)
@@ -50,6 +51,6 @@ class SimCLRTask(AuxiliaryTask):
         x_t = torch.cat([self.augment(x_i) for x_i in x.cpu()], dim=0)   # [2*B, C, H, W]
         h_t = self.encode(x_t.to(self.device)).flatten(start_dim=1)  # [2*B, repr_dim]
         z = self.projector(h_t)  # [2*B, proj_dim]
-        loss = nt_xent_loss(z, self.options.xent_temp)
+        loss = self.loss(z, self.options.xent_temp)
         loss_info = LossInfo(name=self.name, total_loss=loss)
         return loss_info
