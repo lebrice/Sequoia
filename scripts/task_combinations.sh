2f2db92c477ffdaab3c2be4832316fab0c78a74d
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-05-31 14:06:13 -0400

    Added N_JOBS to task_combinations.sh
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- scripts/task_combinations.sh
+++ scripts/task_combinations.sh
@@ -2,7 +2,8 @@
 
 NAME="${1:?'Name must be set'}"
 OUT="$SCRATCH/slurm_out/$NAME/%x-%j.out"
-ARGS=${@:2}
+N_JOBS="${2:?'N_JOBS must be set'}"
+ARGS=${@:3}
 
 echo "Sweep with name '$NAME' and with args '$ARGS'"
 # Create the slurm output dir if it doesn't exist already.
@@ -21,12 +22,11 @@ ROT_ARGS="--rotation.coef 1 --rotation.compare False"
 AE_ARGS="--ae.coef 0.01"
 SIMCLR_ARGS="--simclr.coef 1"
 
-
-sbatch --output $OUT --job-name baseline            ./scripts/sbatch_job.sh --run_name "${NAME}_baseline"                   $ARGS 
-sbatch --output $OUT --job-name rotation            ./scripts/sbatch_job.sh --run_name "${NAME}_rotation_1_nc"              $ARGS $ROT_ARGS
-sbatch --output $OUT --job-name ae                  ./scripts/sbatch_job.sh --run_name "${NAME}_ae_001"                     $ARGS $AE_ARGS
-sbatch --output $OUT --job-name simclr     	        ./scripts/sbatch_job.sh --run_name "${NAME}_simclr_1"                   $ARGS $SIMCLR_ARGS
-sbatch --output $OUT --job-name rotation_ae         ./scripts/sbatch_job.sh --run_name "${NAME}_rotation_1_nc_ae_001"       $ARGS $ROT_ARGS $AE_ARGS
-sbatch --output $OUT --job-name rotation_simclr     ./scripts/sbatch_job.sh --run_name "${NAME}_rotation_1_nc_simclr_1"     $ARGS $ROT_ARGS $SIMCLR_ARGS
-sbatch --output $OUT --job-name vae_simclr          ./scripts/sbatch_job.sh --run_name "${NAME}_ae_001_simclr_1"            $ARGS $AE_ARGS $SIMCLR_ARGS
-sbatch --output $OUT --job-name rotation_ae_simclr  ./scripts/sbatch_job.sh --run_name "${NAME}_rotation_01_nc_ae_001_simclr_1" $ARGS $ROT_ARGS $AE_ARGS $SIMCLR_ARGS
+sbatch --output $OUT --job-name baseline            ./scripts/sbatch_job.sh --run_name "${NAME}_baseline"                    $N_JOBS $ARGS 
+sbatch --output $OUT --job-name rotation            ./scripts/sbatch_job.sh --run_name "${NAME}_rotation_1_nc"               $N_JOBS $ARGS $ROT_ARGS
+sbatch --output $OUT --job-name ae                  ./scripts/sbatch_job.sh --run_name "${NAME}_ae_001"                      $N_JOBS $ARGS $AE_ARGS
+sbatch --output $OUT --job-name simclr     	        ./scripts/sbatch_job.sh --run_name "${NAME}_simclr_1"                    $N_JOBS $ARGS $SIMCLR_ARGS
+sbatch --output $OUT --job-name rotation_ae         ./scripts/sbatch_job.sh --run_name "${NAME}_rotation_1_nc_ae_001"        $N_JOBS $ARGS $ROT_ARGS $AE_ARGS
+sbatch --output $OUT --job-name rotation_simclr     ./scripts/sbatch_job.sh --run_name "${NAME}_rotation_1_nc_simclr_1"      $N_JOBS $ARGS $ROT_ARGS $SIMCLR_ARGS
+sbatch --output $OUT --job-name vae_simclr          ./scripts/sbatch_job.sh --run_name "${NAME}_ae_001_simclr_1"             $N_JOBS $ARGS $AE_ARGS $SIMCLR_ARGS
+sbatch --output $OUT --job-name rotation_ae_simclr  ./scripts/sbatch_job.sh --run_name "${NAME}_rotation_01_nc_ae_001_simclr_1" $N_JOBS $ARGS $ROT_ARGS $AE_ARGS $SIMCLR_ARGS
