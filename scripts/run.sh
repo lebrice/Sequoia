c35457cc9325aad21049bce12b32631454e7ba30
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-06-04 17:56:02 -0400

    removing account from sbatch header
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#SBATCH --account=rrg-bengioy-ad    # Yoshua pays for your job
 #SBATCH --gres=gpu:1                    # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6               # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32G                       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
