e12953f1d2a840c76381617b81a881f5e065cd76
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-06-04 17:36:34 -0400

    Writing in the final_figure.sh
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- scripts/final_figure.sh
+++ scripts/final_figure.sh
@@ -1,6 +1,6 @@
-!/bin/bash
+#!/bin/bash
 
-source ./scripts/task_combinations_ewc.sh \
+source ./scripts/task_combinations_ewc.sh cifar100-20c 5 \
     --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 100 \
     --patience 10 \
     --multihead --no_wandb_cleanup \
@@ -9,3 +9,22 @@ source ./scripts/task_combinations_ewc.sh \
     --n_neighbours 50 \
     --tags cifar100-20c
 
+
+source ./scripts/task_combinations_ewc.sh cifar10 5 \
+    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 100 \
+    --patience 10 \
+    --multihead --no_wandb_cleanup \
+    --dataset cifar10 \
+    --run_group cifar10 \
+    --n_neighbours 50 \
+    --tags cifar10
+
+
+source ./scripts/task_combinations_ewc.sh fashion-mnist 5 \
+    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 100 \
+    --patience 10 \
+    --multihead --no_wandb_cleanup \
+    --dataset fashion-mnist \
+    --run_group cifar10 \
+    --n_neighbours 50 \
+    --tags fashion-mnist
