3cc8cf762d48fe134a8ae890788d219a766bb46b
Fabrice Normandin <normandf@leto06.server.mila.quebec>
2020-06-04 18:05:14 -0400

    Fixed the issue! (sbatch header output field)
    Signed-off-by: Fabrice Normandin <normandf@leto06.server.mila.quebec>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --account=normandf
 #SBATCH -p long
 #SBATCH --gres=gpu:1                    # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6               # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32G                       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=24:00:00                 # The job will run for 24 hours max
-#SBATCH --output /scratch/normandf/slurm_out/%x/%x-%A_%a.out  # Write stdout in $SCRATCH
+#SBATCH --output /network/home/normandf/slurm_out/%x/%x-%A_%a.out  # Write stdout in $SCRATCH
 
 cd $SCRATCH/repos/SSCL
 
