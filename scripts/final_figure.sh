09cc294cfc0b4f755c134d8a7976d50187937e00
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-06-04 17:41:01 -0400

    Fixed typos in final_figure.sh
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- scripts/final_figure.sh
+++ scripts/final_figure.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+
 source ./scripts/task_combinations_ewc.sh cifar100-20c 5 \
     --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 100 \
     --patience 10 \
     --multihead --no_wandb_cleanup \
     --dataset cifar100 --n_classes_per_task 20 \
-    --run_group cifar100-20c \
     --n_neighbours 50 \
     --tags cifar100-20c
 
@@ -15,7 +15,6 @@ source ./scripts/task_combinations_ewc.sh cifar10 5 \
     --patience 10 \
     --multihead --no_wandb_cleanup \
     --dataset cifar10 \
-    --run_group cifar10 \
     --n_neighbours 50 \
     --tags cifar10
 
@@ -25,6 +24,5 @@ source ./scripts/task_combinations_ewc.sh fashion-mnist 5 \
     --patience 10 \
     --multihead --no_wandb_cleanup \
     --dataset fashion-mnist \
-    --run_group cifar10 \
     --n_neighbours 50 \
     --tags fashion-mnist
