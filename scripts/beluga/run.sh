38408cc1244c1b8a808da2b136de57b71d24e4ba
Oleksiy Ostapenko <oleks.ostapenko@icloud.com>
2020-06-05 17:12:41 -0400

    blg

--- scripts/beluga/run.sh
+++ scripts/beluga/run.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:1                    # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6               # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32G                       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=24:00:00                 # The job will run for 24 hours max
+#SBATCH --time=48:00:00                 # The job will run for 24 hours max
 #SBATCH --output /network/home/normandf/slurm_out/%x-%A_%a.out  # Write stdout in $SCRATCH
 
 export TORCH_HOME="$SCRATCH/.torch"
@@ -12,7 +12,8 @@ cd $SCRATCH/repos/SSCL
 
 echo "Slurm Array Job ID: $SLURM_ARRAY_TASK_ID"
 
-source scripts/setup.sh
+#source scripts/setup.sh
+source ~/ENVS/SSCl/bin/activate
 
 function cleanup(){
     echo "Cleaning up and transfering files from $SLURM_TMPDIR to $SCRATCH/SSCL"
--- scripts/final_figure.sh
+++ scripts/final_figure.sh
@@ -4,7 +4,7 @@ export SCRATCH=${SCRATCH:=$HOME}
 
 
 source ./scripts/task_combinations_ewc.sh cifar100-20c 5 \
-    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 100 \
+    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 200 \
     --patience 10 \
     --multihead --no_wandb_cleanup \
     --dataset cifar100 --n_classes_per_task 20 \
@@ -13,7 +13,7 @@ source ./scripts/task_combinations_ewc.sh cifar100-20c 5 \
 
 
 source ./scripts/task_combinations_ewc.sh cifar10 5 \
-    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 100 \
+    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 200 \
     --patience 10 \
     --multihead --no_wandb_cleanup \
     --dataset cifar10 \
@@ -22,7 +22,7 @@ source ./scripts/task_combinations_ewc.sh cifar10 5 \
 
 
 source ./scripts/task_combinations_ewc.sh fashion-mnist 5 \
-    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 100 \
+    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 200 \
     --patience 10 \
     --multihead --no_wandb_cleanup \
     --dataset fashion_mnist \
