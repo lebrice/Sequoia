53815acdff657e94161fad1e82d5ef791a4c06f7
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-06-08 17:12:55 -0400

    Removed early stopping, increased epochs to 200
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- scripts/final_figure.sh
+++ scripts/final_figure.sh
@@ -4,27 +4,23 @@ export SCRATCH=${SCRATCH:=$HOME}
 
 
 source ./scripts/task_combinations_ewc.sh cifar100-20c 5 \
-    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 100 \
-    --patience 10 \
+    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 200 \
     --multihead --no_wandb_cleanup \
     --dataset cifar100 --n_classes_per_task 20 \
     --n_neighbors 50 \
-    --tags cifar100-20c
-
+    --tags cifar100-20c no-early-stopping
 
 source ./scripts/task_combinations_ewc.sh cifar10 5 \
-    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 100 \
-    --patience 10 \
+    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 200 \
     --multihead --no_wandb_cleanup \
     --dataset cifar10 \
     --n_neighbors 50 \
-    --tags cifar10
-
+    --tags cifar10 no-early-stopping
 
 source ./scripts/task_combinations_ewc.sh fashion-mnist 5 \
-    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 100 \
-    --patience 10 \
+    --unsupervised_epochs_per_task 0 --supervised_epochs_per_task 200 \
     --multihead --no_wandb_cleanup \
     --dataset fashion_mnist \
     --n_neighbors 50 \
-    --tags fashion-mnist
+    --tags fashion-mnist no-early-stopping
+
