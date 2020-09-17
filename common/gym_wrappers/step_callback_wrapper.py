11d99882bed9cb740c170d232466b9eb430a5193
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-09-16 21:20:31 -0400

    Added a Wrapper for executing callbacks at steps
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

---
+++ common/gym_wrappers/step_callback_wrapper.py
@@ -0,0 +1,83 @@
+"""TODO: Make a wrapper that calls a given function/callback when a given step is reached.
+"""
+from abc import ABC, abstractmethod
+from collections import OrderedDict, defaultdict
+from typing import Callable, Dict, List, Tuple, Union
+
+import gym
+
+
+class Callback(Callable[[int, gym.Env], None], ABC):
+    @abstractmethod
+    def __call__(self, step: int, env: gym.Env, step_results: Tuple) -> None:
+        raise NotImplementedError()
+
+
+class StepCallback(Callback, ABC):
+    def __init__(self, step: int, func: Callable[[int, gym.Env, Tuple], None] = None):
+        self.step = step
+        self.func = func
+
+    def __call__(self, step: int, env: gym.Env, step_results: Tuple) -> None:
+        if self.func:
+            return self.func(step, env, step_results)
+        raise NotImplementedError("Create your own callback or pass a func to use.")
+
+class PeriodicCallback(Callback):
+    def __init__(self,
+                 period: int,
+                 offset: int = 0,
+                 func: Callable[[int, gym.Env], None] = None):
+        self.period = period
+        self.offset = offset
+        self.func = func
+    
+    def __call__(self, step: int, env: gym.Env, step_results: Tuple) -> None:
+        if self.func:
+            return self.func(step, env, step_results)
+        raise NotImplementedError("Create your own callback or pass a func to use.")
+
+
+class StepCallbackWrapper(gym.Wrapper):
+    def __init__(self,
+                 env: gym.Env,
+                 callbacks: List[Callback] = None,
+                 ):
+        super().__init__(env)
+        self._steps = 0
+        self.callbacks = callbacks or []
+
+    def add_callback(self, callback: Union[Callback]) -> None:
+        self.callbacks.append(callback)
+
+    def add_step_callback(self, step: int, callback: Callable[[int, gym.Env], None]):
+        if isinstance(callback, StepCallback):
+            assert step == callback.step
+        else:
+            callback = StepCallback(step=step, func=callback)
+        self.add_callback(callback)
+
+    def add_periodic_callback(self, period: int, callback: StepCallback, offset: int = 0):
+        if isinstance(callback, PeriodicCallback):
+            assert period == callback.period
+            assert offset == callback.offset
+        else:
+            callback = PeriodicCallback(period=period, offset=offset, func=callback)
+        self.add_callback(callback)
+
+    def step(self, action):
+        step_results = super().step(action)
+        for callback in self.callbacks:
+            if isinstance(callback, StepCallback):
+                if callback.step == self._steps:
+                    callback(self._steps, self, step_results)
+            elif isinstance(callback, PeriodicCallback):
+                if self._steps >= callback.offset and (self._steps - callback.offset) % callback.period == 0:
+                    callback(self._steps, self, step_results)
+            else:
+                # if it's a callable, just call it all the time, assuming that
+                # it will use some condition in it's __call__ to check wether
+                # it should be executed or not.
+                callback(self._steps, self, step_results)
+        self._steps += 1
+        return step_results
---
+++ common/gym_wrappers/step_callback_wrapper_test.py
@@ -0,0 +1,66 @@
+from typing import Tuple
+
+import gym
+
+from .step_callback_wrapper import (Callback, PeriodicCallback, StepCallback,
+                                    StepCallbackWrapper)
+
+i: int = 0
+
+def increment_i(step: int, env: gym.Env, step_results: Tuple):
+    global i
+    print(f"Incrementing i at step {step}: ({i} -> {i+1})")
+    i += 1
+
+def decrement_i(step: int, env: gym.Env, step_results: Tuple):
+    global i
+    print(f"Decrementing i at step {step}: ({i} -> {i-1})")
+    i -= 1
+
+
+def test_step_callback():
+    callback = StepCallback(step=7, func=increment_i)
+    env = StepCallbackWrapper(gym.make("CartPole-v0"), callbacks=[callback])
+    env.reset()
+    global i
+    i = 0
+    for step in range(10):
+        obs, reward, done, info = env.step(env.action_space.sample())
+
+        if step < 7:
+            assert i == 0
+        else:
+            assert i == 1
+        if done:
+            env.reset()
+    env.close()
+
+
+def test_periodic_callback():
+    global i
+    i = 0
+    inc_callback = PeriodicCallback(period=5, func=increment_i)
+    dec_callback = PeriodicCallback(period=5, func=decrement_i, offset=2)
+    env = StepCallbackWrapper(gym.make("CartPole-v0"), callbacks=[inc_callback, dec_callback])
+    env.reset()
+
+    def _next(env) -> int:
+        obs, reward, done, info = env.step(env.action_space.sample())
+        if done:
+            env.reset()
+        return i
+
+    assert _next(env) == 1
+    assert _next(env) == 1
+    assert _next(env) == 0
+    assert _next(env) == 0
+    assert _next(env) == 0
+
+
+    assert _next(env) == 1
+    assert _next(env) == 1
+    assert _next(env) == 0
+    assert _next(env) == 0
+    assert _next(env) == 0
+
+    env.close()
