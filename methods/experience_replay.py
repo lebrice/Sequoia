9acc09319d6155972df345abfbd05645c0f45ae8
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-11-24 14:31:42 -0500

    Minor changes, add seeding & types to functions
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- methods/experience_replay.py
+++ methods/experience_replay.py
@@ -4,7 +4,7 @@ Should be applicable to any Setting.
 """
 from collections import Iterable, OrderedDict
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Tuple, Dict, Type
 from argparse import ArgumentParser, Namespace
 
 import gym
@@ -34,47 +34,54 @@ class ExperienceReplayMethod(Method, target_setting=ClassIncrementalSetting):
     """ Simple method that uses a replay buffer to reduce forgetting.
     """
     
-    def __init__(self, learning_rate: float = 0.1, buffer_capacity: int = 200):
+    def __init__(self,
+                 learning_rate: float = 0.1,
+                 buffer_capacity: int = 200,
+                 seed: int = None):
         self.learning_rate = learning_rate
         self.buffer_capacity = buffer_capacity
 
         self.net: ResNet
-        self.buffer: Buffer
+        self.buffer: Optional[Buffer] = None
         self.optim: torch.optim.Optimizer
         self.task: int = 0
+        self.rng = np.random.RandomState(seed)
+        if seed:
+            torch.manual_seed(seed)
+            torch.set_deterministic(True)
         
-        self.device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.epochs_per_task: int = 0
+        
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
 
     def configure(self, setting: ClassIncrementalSetting):
         # create the model
         self.net = models.resnet18(pretrained=False)
-        self.net.fc = nn.Linear(512, setting.increment * setting.nb_tasks)
+        self.net.fc = nn.Linear(512, setting.num_classes)
         if torch.cuda.is_available():
             self.net = self.net.to(device=self.device)
-        net_params = sum([np.prod(x.shape) for x in self.net.parameters()])
 
         image_space: spaces.Box = setting.observation_space[0]
         # create the buffer
-        self.buffer = Buffer(capacity=self.buffer_capacity, input_shp=image_space.shape)
-        self.buffer = self.buffer.to(device=self.device)
-
+        
+        if self.buffer_capacity:    
+            self.buffer = Buffer(
+                capacity=self.buffer_capacity,
+                input_shape=image_space.shape,
+                extra_buffers={"t": torch.LongTensor},
+                rng=self.rng
+            ).to(device=self.device)
         # optimizer
         self.optim = torch.optim.SGD(self.net.parameters(), lr=self.learning_rate)
 
-    def fit(self,
-            train_env: Environment=None,
-            valid_env: Environment=None,
-            datamodule=None
-        ):
+    def fit(self, train_env: Environment, valid_env: Environment):
         self.net.train()
-
-        # Training loop:
+        # Simple example training loop, not using the validation loader.
         with tqdm.tqdm(train_env) as train_pbar:
             postfix = {}
             train_pbar.set_description(f"Training")
             for i, batch in enumerate(train_pbar):
-
                 self.optim.zero_grad()
 
                 obs, rew = batch
@@ -84,7 +91,7 @@ class ExperienceReplayMethod(Method, target_setting=ClassIncrementalSetting):
                 logits = self.net(x)
                 loss = F.cross_entropy(logits, y)
 
-                if self.task > 0:
+                if self.task > 0 and self.buffer:
                     b_samples = self.buffer.sample(x.size(0))
                     b_logits = self.net(b_samples['x'])
                     loss += F.cross_entropy(logits, b_samples['y'])
@@ -93,77 +100,80 @@ class ExperienceReplayMethod(Method, target_setting=ClassIncrementalSetting):
                 self.optim.step()
 
                 # add to buffer
-                self.buffer.add_reservoir({'x': x, 'y': y, 't': self.task})
+                if self.buffer:
+                    self.buffer.add_reservoir({'x': x, 'y': y, 't': self.task})
 
     def get_actions(self, observations: Observations, action_space: gym.Space) -> Actions:
         observations = observations.to(device=self.device)
         logits = self.net(observations.x)
-        pred   = logits.max(1)[1]
+        pred = logits.max(1)[1]
         return pred
 
     def on_task_switch(self, task_id: Optional[int]):
+        print(f"Switching from task {self.task} to task {task_id}")
         self.task = task_id
-        print(f'task id : {task_id}')
 
     @classmethod
     def add_argparse_args(cls, parser: ArgumentParser, dest: str = None) -> None:
-        parser.add_argument("--learning_rate", type=float, default=0.1)
-        parser.add_argument("--buffer_capacity", type=int, default=200)
+        """Add the command-line arguments for this Method to the given parser.
+        
+        Parameters
+        ----------
+        parser : ArgumentParser
+            The ArgumentParser. 
+        dest : str, optional
+            The 'base' destination where the arguments should be set on the
+            namespace, by default None, in which case the arguments can be at
+            the "root" level on the namespace.
+        """
+        prefix = f"{dest}." if dest else ""
+        parser.add_argument(f"--{prefix}learning_rate", type=float, default=0.1)
+        parser.add_argument(f"--{prefix}buffer_capacity", type=int, default=200)
+        parser.add_argument(f"--{prefix}seed", type=int, default=None, help="Random seed")
     
     @classmethod
     def from_argparse_args(cls, args: Namespace, dest: str = None):
-        return cls(learning_rate=args.learning_rate,
-                   buffer_capacity=args.buffer_capacity)
-        return super().from_argparse_args(args, dest=dest)
-    
-    @singledispatchmethod
-    def validate_results(self, setting: Setting, results: Setting.Results):
-        """Called during testing. Use this to assert that the results you get
-        from applying your method on the given setting match your expectations.
+        """Extract the parsed command-line arguments from the namespace and
+        return an instance of class `cls`.
 
-        Args:
-            setting
-            results (Results): A given Results object.
+        Parameters
+        ----------
+        args : Namespace
+            The namespace containing all the parsed command-line arguments.
+        dest : str, optional
+            The , by default None
+
+        Returns
+        -------
+        cls
+            An instance of the class `cls`.
         """
-        assert results is not None
-        assert results.objective > 0
-        print(f"Objective when applied to a setting of type {type(setting)}: {results.objective}")
-
-    @validate_results.register
-    def validate(self, setting: ClassIncrementalSetting, results: ClassIncrementalSetting.Results):
-        assert isinstance(setting, ClassIncrementalSetting), setting
-        assert isinstance(results, ClassIncrementalSetting.Results), results
-
-        average_accuracy = results.objective
-        # Calculate the expected 'average' chance accuracy.
-        # We assume that there is an equal number of classes in each task.
-        chance_accuracy = 1 / setting.n_classes_per_task
-
-        assert 0.5 * chance_accuracy <= average_accuracy <= 1.5 * chance_accuracy
-
-        for i, metric in enumerate(results.average_metrics_per_task):
-            assert isinstance(metric, ClassificationMetrics)
-            # TODO: Check that this makes sense:
-            chance_accuracy = 1 / setting.n_classes_per_task
-
-            task_accuracy = metric.accuracy
-            # FIXME: Look into this, we're often getting results substantially
-            # worse than chance, and to 'make the tests pass' (which is bad)
-            # we're setting the lower bound super low, which makes no sense.
-            assert 0.25 * chance_accuracy <= task_accuracy <= 2.1 * chance_accuracy
+        args = args if not dest else getattr(args, dest)
+        return cls(
+            learning_rate=args.learning_rate,
+            buffer_capacity=args.buffer_capacity,
+            seed=args.seed,
+        )
 
 
 class Buffer(nn.Module):
-    def __init__(self, capacity, input_shp, extra_buffers={'t': torch.LongTensor}):
+    def __init__(self,
+                 capacity: int,
+                 input_shape: Tuple[int, ...],
+                 extra_buffers: Dict[str, Type[torch.Tensor]] = None,
+                 rng: np.random.RandomState = None,
+                 ):
         super().__init__()
+        self.rng = rng or np.random.RandomState()
 
-        bx = torch.FloatTensor(capacity, *input_shp).fill_(0)
-        by = torch.LongTensor(capacity).fill_(0)
+        bx = torch.zeros([capacity, *input_shape], dtype=torch.float)
+        by = torch.zeros([capacity], dtype=torch.long)
 
         self.register_buffer('bx', bx)
         self.register_buffer('by', by)
         self.buffers = ['bx', 'by']
 
+        extra_buffers = extra_buffers or {}
         for name, dtype in extra_buffers.items():
             tmp = dtype(capacity).fill_(0)
             self.register_buffer(f'b{name}', tmp)
@@ -173,7 +183,7 @@ class Buffer(nn.Module):
         self.n_seen_so_far = 0
         self.is_full       = 0
         # (@lebrice) args isn't defined here:
-        self.to_one_hot  = lambda x : x.new(x.size(0), args.n_classes).fill_(0).scatter_(1, x.unsqueeze(1), 1)
+        # self.to_one_hot  = lambda x : x.new(x.size(0), args.n_classes).fill_(0).scatter_(1, x.unsqueeze(1), 1)
         self.arange_like = lambda x : torch.arange(x.size(0)).to(x.device)
         self.shuffle     = lambda x : x[torch.randperm(x.size(0))]
 
@@ -183,9 +193,10 @@ class Buffer(nn.Module):
 
     @property
     def y(self):
+        raise NotImplementedError("Can't make y one-hot, dont have n_classes.")
         return self.to_one_hot(self.by[:self.current_index])
 
-    def add_reservoir(self, batch):
+    def add_reservoir(self, batch: Dict[str, Tensor]) -> None:
         n_elem = batch['x'].size(0)
 
         # add whatever still fits in the buffer
@@ -226,17 +237,12 @@ class Buffer(nn.Module):
         for name, data in batch.items():
             buffer = getattr(self, f'b{name}')
             if isinstance(data, Iterable):
-                try:
-                    data = data[place_left:]
-                    buffer[idx_buffer] = data[idx_new_data]
-                except:
-                    import pdb; pdb.set_trace()
-                    xx = 2
+                data = data[place_left:]
+                buffer[idx_buffer] = data[idx_new_data]
             else:
                 buffer[idx_buffer] = data
 
-
-    def sample(self, amt, exclude_task=None):
+    def sample(self, n_samples: int, exclude_task: int = None) -> Dict[str, Tensor]:
         buffers = OrderedDict()
         if exclude_task is not None:
             assert hasattr(self, 'bt')
@@ -248,11 +254,12 @@ class Buffer(nn.Module):
                 buffers[buffer_name] = getattr(self, buffer_name)[:self.current_index]
 
         bx = buffers['bx']
-        if bx.size(0) < amt:
+        if bx.size(0) < n_samples:
             return buffers
         else:
-            indices = torch.from_numpy(np.random.choice(bx.size(0), amt, replace=False)).to(self.bx.device)
-            return OrderedDict({k[1:]:v[indices] for (k,v) in buffers.items()})
+            indices_np = self.rng.choice(bx.size(0), n_samples, replace=False)
+            indices = torch.from_numpy(indices_np).to(self.bx.device)
+            return OrderedDict({k[1:]: v[indices] for (k,v) in buffers.items()})
 
 
 if __name__ == "__main__":
