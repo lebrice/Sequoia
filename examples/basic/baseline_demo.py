73a3227ae7a6f6d6f4b3316975f29efb98c739d6
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-01-12 14:45:21 -0500

    Adding test for baseline_demo.py
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- examples/basic/baseline_demo.py
+++ examples/basic/baseline_demo.py
@@ -4,21 +4,22 @@ RL and SL settings.
 
 from sequoia.methods import BaselineMethod
 from sequoia.settings import TaskIncrementalSetting, TaskIncrementalRLSetting
-
+from sequoia.common import Config
 
 
 if __name__ == "__main__":
-    setting = TaskIncrementalRLSetting(
-        dataset="cartpole",
-        observe_state_directly=True,
-        max_steps=4000,
+    # setting = TaskIncrementalRLSetting(
+    #     dataset="cartpole",
+    #     observe_state_directly=True,
+    #     max_steps=4000,
+    #     nb_tasks=2,
+    # )
+    # TODO: The length of each epoch doesn't show up in the progressbar anymore.
+    setting = TaskIncrementalSetting(
+        dataset="cifar10",
         nb_tasks=2,
     )
-    # setting = TaskIncrementalSetting(
-    #     dataset="cifar10",
-    # )
-    
-    method = BaselineMethod()
-    
-    results = setting.apply(method)
+    config = Config(render=True, debug=True)
+    method = BaselineMethod(config=config, max_epochs=1)
+    results = setting.apply(method, config=config)
     print(results.summary())
