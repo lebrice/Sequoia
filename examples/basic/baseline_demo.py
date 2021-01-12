55460226ff2cb2e2f3fb1af79ab26ad21abc9d3d
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-01-12 15:15:55 -0500

    Fixing the links in the examples README
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- examples/basic/baseline_demo.py
+++ examples/basic/baseline_demo.py
@@ -6,20 +6,27 @@ from sequoia.methods import BaselineMethod
 from sequoia.settings import TaskIncrementalSetting, TaskIncrementalRLSetting
 from sequoia.common import Config
 
+def demo_rl():
+    
+
+
+
 
 if __name__ == "__main__":
-    # setting = TaskIncrementalRLSetting(
-    #     dataset="cartpole",
-    #     observe_state_directly=True,
-    #     max_steps=4000,
-    #     nb_tasks=2,
-    # )
     # TODO: The length of each epoch doesn't show up in the progressbar anymore.
-    setting = TaskIncrementalSetting(
-        dataset="cifar10",
-        nb_tasks=2,
-    )
     config = Config(render=True, debug=True)
     method = BaselineMethod(config=config, max_epochs=1)
+    
+    # setting = TaskIncrementalSetting(
+    #     dataset="cifar10",
+    #     nb_tasks=2,
+    # )
+    setting = TaskIncrementalRLSetting(
+        dataset="cartpole",
+        observe_state_directly=True,
+        max_steps=4000,
+        nb_tasks=2,
+    )
+
     results = setting.apply(method, config=config)
     print(results.summary())
