a7f127169da4e86523a693d752cd5ee377bfd5e3
Julio Hurtado <julious.hurtado@gmail.com>
2021-01-21 19:34:33 -0300

    Minor fix in PNN RL

--- examples/advanced/pnn_demo_rl.py
+++ examples/advanced/pnn_demo_rl.py
@@ -11,26 +11,23 @@ from gym import spaces
 
 from scipy.signal import lfilter
 
-from sequoia.settings.active.continual import ContinualRLSetting
 from sequoia.settings import TaskIncrementalRLSetting
-
 from stable_baselines3.common.base_class import BaseAlgorithm
 from sequoia.settings import Method
-from sequoia.methods.stable_baselines3_methods import A2CModel
-from sequoia.methods.stable_baselines3_methods import StableBaselines3Method
 
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import transforms
 
 class PNNConvLayer(nn.Module):
-    def __init__(self, col, depth, n_in, n_out):
+    def __init__(self, col, depth, n_in, n_out, kernel_size=3):
         super(PNNConvLayer, self).__init__()
         self.col = col
-        self.layer = nn.Conv2d(n_in, n_out, 3, stride=2, padding=1)
+        self.layer = nn.Conv2d(n_in, n_out, kernel_size, stride=2, padding=1)
 
         self.u = nn.ModuleList()
         if depth > 0:
-            self.u.extend([ nn.Conv2d(n_in, n_out, 3, stride=2, padding=1) for _ in range(col) ])
+            self.u.extend([ nn.Conv2d(n_in, n_out, kernel_size, stride=2, padding=1) for _ in range(col) ])
 
     def forward(self, inputs):
         if not isinstance(inputs, list):
@@ -39,7 +36,7 @@ class PNNConvLayer(nn.Module):
         cur_column_out = self.layer(inputs[-1])
         prev_columns_out = [mod(x) for mod, x in zip(self.u, inputs)]
 
-        return F.elu(cur_column_out + sum(prev_columns_out))
+        return F.relu(cur_column_out + sum(prev_columns_out))
 
 class PNNGruLayer(nn.Module):
     def __init__(self, col, depth, n_in, n_out):
@@ -89,15 +86,23 @@ class PNN(nn.Module):
         super(PNN, self).__init__()
         self.columns_actor = nn.ModuleList([])
         self.columns_critic = nn.ModuleList([])
+        self.columns_conv = nn.ModuleList([])
         self.arch = arch
         self.hidden_size = hidden_size
 
-    def forward(self, observations, hx):
+        # Original size 3 x 400 x 600
+        self.transformation = transforms.Compose([transforms.ToPILImage(),
+                                        transforms.Resize(256),
+                                        transforms.CenterCrop(224), 
+                                        transforms.ToTensor(), 
+                                        ])
+
+    def forward(self, observations):
         assert self.columns_actor, 'PNN should at least have one column (missing call to `new_task` ?)'
-        x = torch.from_numpy(observations.x).unsqueeze(0).float()
         t = observations.task_labels
 
         if self.arch == 'mlp':
+            x = torch.from_numpy(observations.x).unsqueeze(0).float()
             inputs_critic = [c[1](c[0](x)) for c in self.columns_critic]
             inputs_actor = [c[1](c[0](x)) for c in self.columns_actor]
 
@@ -107,29 +112,31 @@ class PNN(nn.Module):
                 outputs_critic.append(column[2](inputs_critic[:i+1]))
                 outputs_actor.append(self.columns_actor[i][2](inputs_actor[:i+1]))
 
-            #for l in range(1, 2):
-            #    outputs = []
-            #    for i, column in enumerate(self.columns):
-            #        outputs.append(column[l](inputs[:i+1]))
-
             ind_depth = 3
 
         else:
-            pass
-            # hx = [ hx ]
-            # inputs = [c[0](x) for c in self.columns]
-            # for l in range(1, 3):
-            #     outputs = []
-            #     for i, column in enumerate(self.columns):
-            #         outputs.append(column[l](inputs[:i+1]))
+            x = self.transfor_img(observations.x).unsqueeze(0).float()
+            inputs = [c[1](c[0](x)) for c in self.columns_conv]
 
-            # inputs = [ torch.flatten(o, start_dim=1) for o in outputs ]    
-            # outputs = []
-            # for i, column in enumerate(self.columns):
-            #     hx = column[4](inputs[i], hx)
-            #     outputs.append(hx)
-            # inputs = outputs
-            # ind_depth = 5
+            outputs = []
+            for i, column in enumerate(self.columns_conv):
+                outputs.append(column[3](column[2](inputs[:i+1])))
+
+            inputs = outputs
+            outputs = []
+            for i, column in enumerate(self.columns_conv):
+                outputs.append(column[5](column[4](inputs[:i+1])))
+
+            inputs_critic = [ c[6](outputs[i]).view(1,-1) for i,c in enumerate(self.columns_conv) ]
+            inputs_actor = inputs_critic[:]
+
+            outputs_critic = []
+            outputs_actor = []
+            for i, column in enumerate(self.columns_critic):
+                outputs_critic.append(column[0](inputs_critic[:i+1]))
+                outputs_actor.append(self.columns_actor[i][0](inputs_actor[:i+1]))
+
+            ind_depth = 1
 
         critic = []
         for i, column in enumerate(self.columns_critic):
@@ -139,36 +146,38 @@ class PNN(nn.Module):
         for i, column in enumerate(self.columns_actor):
             actor.append(F.softmax(column[ind_depth](outputs_actor[i]),dim=1))
 
-        return critic[t], actor[t], None #outputs[-1]
+        return critic[t], actor[t]
 
     def new_task(self, device, num_inputs, num_actions = 5):
         task_id = len(self.columns_actor)
 
-        if self.arch == 'mlp':
-            modules_actor = nn.Sequential()
-            modules_critic = nn.Sequential()
+        if self.arch == 'conv':
+            sizes = [num_inputs, 32, 64, self.hidden_size]
+            modules_conv = nn.Sequential()
 
+            modules_conv.add_module('Conv1', PNNConvLayer(task_id, 0, sizes[0], sizes[1]))
+            modules_conv.add_module('MaxPool1', nn.MaxPool2d(3))
+            modules_conv.add_module('Conv2', PNNConvLayer(task_id, 1, sizes[1], sizes[2]))
+            modules_conv.add_module('MaxPool2', nn.MaxPool2d(3))
+            modules_conv.add_module('Conv3', PNNConvLayer(task_id, 2, sizes[2], sizes[3]))
+            modules_conv.add_module('MaxPool3', nn.MaxPool2d(3))
+            modules_conv.add_module('globavgpool2d', nn.AdaptiveAvgPool2d((1,1)))
+            self.columns_conv.append(modules_conv)
+
+        modules_actor = nn.Sequential()
+        modules_critic = nn.Sequential()
+
+        if self.arch == 'mlp':
             modules_actor.add_module('linAc1',nn.Linear(num_inputs, self.hidden_size))
             modules_actor.add_module('relAc',nn.ReLU(inplace=True))
-            modules_actor.add_module('linAc2',PNNLinearBlock(task_id, 1, self.hidden_size, self.hidden_size))
-            modules_actor.add_module('linAc3',nn.Linear(self.hidden_size, num_actions))
+        modules_actor.add_module('linAc2',PNNLinearBlock(task_id, 1, self.hidden_size, self.hidden_size))
+        modules_actor.add_module('linAc3',nn.Linear(self.hidden_size, num_actions))
 
+        if self.arch == 'mlp':
             modules_critic.add_module('linCr1',nn.Linear(num_inputs, self.hidden_size))
             modules_critic.add_module('relCr',nn.ReLU(inplace=True))
-            modules_critic.add_module('linCr2',PNNLinearBlock(task_id, 1, self.hidden_size, self.hidden_size))
-            modules_critic.add_module('linCr3',nn.Linear(self.hidden_size, 1))
-
-        else:
-            pass
-            # sizes = [1, 32, 32, 32, 32, 32 * 5 * 5, 256]
-            # modules = [ PNNConvLayer(task_id, 0, sizes[0], sizes[1]) ]
-
-            # for depth in range(2,4):
-            #     modules.append(PNNConvLayer(task_id, depth, sizes[depth-1], sizes[depth]))
-
-            # modules.append(PNNGruLayer(task_id, 4, sizes[5], sizes[6]))
-            # modules.append(PNNLinearBlock(task_id, 5, sizes[6], 1))
-            # modules.append(PNNLinearBlock(task_id, 5, sizes[6], num_actions))
+        modules_critic.add_module('linCr2',PNNLinearBlock(task_id, 1, self.hidden_size, self.hidden_size))
+        modules_critic.add_module('linCr3',nn.Linear(self.hidden_size, 1))
 
         self.columns_actor.append(modules_actor)
         self.columns_critic.append(modules_critic)
@@ -187,6 +196,11 @@ class PNN(nn.Module):
                 for params in self.columns_critic[i].parameters():
                     params.requires_grad = False
 
+        for i, c in enumerate(self.columns_conv):
+            if i not in skip:
+                for params in c.parameters():
+                    params.requires_grad = False
+
         print("Freeze columns from previous tasks")
 
     def parameters(self):
@@ -196,24 +210,36 @@ class PNN(nn.Module):
         for p in self.columns_actor[-1].parameters():
             param.append(p)
 
+        if len(self.columns_conv) > 0:
+            for p in self.columns_conv[-1].parameters():
+                param.append(p)
+
         return param
 
     def transfor_img(self, img):
-        return lambda img: imresize(img[35:195].mean(2), (80,80)).astype(np.float32).reshape(1,80,80)/255.
+        return self.transformation(img)
+        # return lambda img: imresize(img[35:195].mean(2), (80,80)).astype(np.float32).reshape(1,80,80)/255.
 
+class ImproveMethod(Method, target_setting=TaskIncrementalRLSetting):
 
+    @dataclass
+    class HParams:
+        """ Hyper-parameters of the Settings. """
+        # Learning rate of the optimizer.
+        learning_rate: float = 3e-4
+        num_steps: int = 200
+        loss_gamma: float = 0.99
+        hidden_size: int = 256
 
-# class PNNMethod(Method, target_setting=Setting):
-#     def configure(self, setting: Setting):
-#         if isinstance(setting, ContinualRLSetting):
-#             self.model = PNNA2CModel()
-#         else:
-#             self.model = PNNResNetClassifier()
+    def __init__(self, hparams: HParams = None):
+        self.hparams: ImproveMethod.HParams = hparams or self.HParams.from_args()
+        
+        # We will create those when `configure` will be called, before training.
+        self.model: PNN
+        self.optimizer: torch.optim.Optimizer
+        self.config: Optional[Config] = None
 
-
-class ImproveMethod(Method, target_setting=ContinualRLSetting):
-
-    def configure(self, setting: ContinualRLSetting):
+    def configure(self, setting: TaskIncrementalRLSetting):
         # Delete the model, if present.
         self.model = None
         setting.batch_size = None
@@ -224,23 +250,27 @@ class ImproveMethod(Method, target_setting=ContinualRLSetting):
 
         self.device = torch.device("cpu" if torch.cuda.is_available() else "cpu")
 
-        self.learning_rate = 3e-4
-        self.train_steps_per_task = 500 #setting.max_steps
-        self.num_steps = 200
+        self.train_steps_per_task = setting.max_steps
+        self.num_steps = self.hparams.num_steps
         # Otherwise, we can train basically as long as we want on each task.
 
         self.loss_function = {
-            'gamma': 0.99,
+            'gamma': self.hparams.loss_gamma,
         }
 
+        if setting.observe_state_directly:
+            self.arch = 'mlp'
+            self.num_inputs = np.prod(image_space.shape)
+        else:
+            self.arch = 'conv'
+            self.num_inputs = image_space.shape[0]
+
         self.task_id = 0
-        self.arch = 'mlp'
-        self.hidden_size = 256
+        self.hidden_size = self.hparams.hidden_size
 
     def create_model(self, train_env: gym.Env, valid_env: gym.Env) -> BaseAlgorithm:
         # Create the model, as usual:
         model = PNN(self.arch, self.hidden_size)
-        self.task_trained = []
         return model
 
     def on_task_switch(self, task_id: Optional[int]) -> None:
@@ -248,31 +278,20 @@ class ImproveMethod(Method, target_setting=ContinualRLSetting):
         self.task_id = task_id
 
     def set_optimizer(self):
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hparams.learning_rate)
 
     def get_actions(self,
-                    observations: ContinualRLSetting.Observations,
-                    action_space: spaces.Space) -> ContinualRLSetting.Actions:
-        # obs = observations.x
-        hx = torch.zeros(1, 256)
-        predictions = self.model(observations, hx)
-        _, logit, _ = predictions
+                    observations: TaskIncrementalRLSetting.Observations,
+                    action_space: spaces.Space) -> TaskIncrementalRLSetting.Actions:
+
+        predictions = self.model(observations)
+        _, logit = predictions
         action = torch.argmax(logit).item()
         return action
 
     def print_results(self, step, loss, reward):
         print("Task: {}\n Step:{}\n Average Loss: {}\n Average Reward: {}\n".format(self.task_id,
                             step, loss, reward))
-    
-    # def fit(self, train_env: gym.Env, valid_env: gym.Env):
-    #     if self.model is None:
-    #         self.model = self.create_model(train_env, valid_env)
-
-    #     self.model.freeze_columns()
-    #     self.model.new_task(self.device, self.num_actions)
-    #     self.set_optimizer()
-    #     # self.model.float()
-    
     
     def fit(self, train_env: gym.Env, valid_env: gym.Env):
         if self.model is None:
@@ -294,22 +313,16 @@ class ImproveMethod(Method, target_setting=ContinualRLSetting):
             log_probs = []
 
             state = train_env.reset()
-            hx = torch.zeros(1, 256)
             for steps in range(self.num_steps):
-                # value, policy_dist = actor_critic.forward(state)
-                value, policy_dist, hx = self.model(state, hx)
+                value, policy_dist = self.model(state)
 
-                value = value.item() #detach().numpy()[0,0]
+                value = value.item() 
                 dist = policy_dist.detach().numpy() 
 
                 action = np.random.choice(self.num_actions, p=np.squeeze(dist))
                 log_prob = torch.log(policy_dist.squeeze(0)[action])
                 entropy = -np.sum(np.mean(dist) * np.log(dist))
                 new_state, reward, done, _ = train_env.step(action)
-                # state = train_env.step(action.item())
-                # reward = env.reward.y
-                # done = env.done.item()
-                # env = env.state
 
                 rewards.append(reward.y)
                 values.append(value)
@@ -318,8 +331,8 @@ class ImproveMethod(Method, target_setting=ContinualRLSetting):
                 state = new_state
 
                 if done or steps == self.num_steps-1:
-                    Qval, _, _ = self.model(state, hx)
-                    Qval = Qval.item() #.detach().numpy()[0,0]
+                    Qval, _ = self.model(state)
+                    Qval = Qval.item() 
                     all_rewards.append(np.sum(rewards))
                     all_lengths.append(steps)
                     average_lengths.append(np.mean(all_lengths[-10:]))
@@ -347,9 +360,9 @@ class ImproveMethod(Method, target_setting=ContinualRLSetting):
             self.optimizer.step()
 
 if __name__ == "__main__":
-    # Stages:
-    ## 1. Creating the setting:
-    from sequoia.settings import RLSetting
+    parser = ArgumentParser(description=__doc__, add_dest_to_option_strings=False)
+    # Haven't tested with observe_state_directly=False 
+    # it run but I don't know if it converge
     setting = TaskIncrementalRLSetting(
         dataset="cartpole",
         observe_state_directly=True,
