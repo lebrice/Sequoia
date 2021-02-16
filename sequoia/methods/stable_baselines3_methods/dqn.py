30a6643d9c6fb950639ad23209d280008518f138
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-02-15 22:00:06 -0500

    Adds hparams and priors for the DQN method
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- sequoia/methods/stable_baselines3_methods/dqn.py
+++ sequoia/methods/stable_baselines3_methods/dqn.py
@@ -11,6 +11,7 @@ from gym.spaces.utils import flatten_space
 from simple_parsing import mutable_field
 from stable_baselines3.dqn import DQN
 
+from sequoia.common.hparams import uniform, log_uniform, categorical
 from sequoia.methods import register_method
 from sequoia.methods.stable_baselines3_methods.base import (
     SB3BaseHParams, StableBaselines3Method)
@@ -29,25 +30,25 @@ class DQNModel(DQN):
         """
         # The learning rate, it can be a function of the current progress (from
         # 1 to 0)
-        learning_rate: Union[float, Callable] = 1e-4
+        learning_rate: Union[float, Callable] = log_uniform(1e-6, 1e-2, default=1e-4)
         # size of the replay buffer
-        buffer_size: int = 1000000
+        buffer_size: int = uniform(100, 10_000_000, default=1_000_000)
         # How many steps of the model to collect transitions for before learning
-        # starts
-        learning_starts: int = 50000
+        # starts.
+        learning_starts: int = uniform(1_000, 100_000, default=50_000)
         # Minibatch size for each gradient update
-        batch_size: Optional[int] = 32
+        batch_size: Optional[int] = categorical(1, 2, 4, 8, 16, 32, 128, default=32)
         # The soft update coefficient ("Polyak update", between 0 and 1) default
         # 1 for hard update
-        tau: float = 1.0
+        tau: float = uniform(0., 1., default=1.0)
         # The discount factor
-        gamma: float = 0.99
+        gamma: float = uniform(0.9, 0.9999, default=0.99)
         # Update the model every ``train_freq`` steps. Set to `-1` to disable.
-        train_freq: int = 4
+        train_freq: int = uniform(1, 100, default=4)
         # How many gradient steps to do after each rollout (see ``train_freq``
         # and ``n_episodes_rollout``) Set to ``-1`` means to do as many gradient
         # steps as steps done in the environment during the rollout.
-        gradient_steps: int = 1
+        gradient_steps: int = categorical(1, -1, default=1)
         # Update the model every ``n_episodes_rollout`` episodes. Note that this
         # cannot be used at the same time as ``train_freq``. Set to `-1` to
         # disable.
@@ -58,16 +59,16 @@ class DQNModel(DQN):
         optimize_memory_usage: bool = False
         # Update the target network every ``target_update_interval`` environment
         # steps.
-        target_update_interval: int = 10000
+        target_update_interval: int = categorical(1_000, 5_000, 10_000, 50_000, default=10_000)
         # Fraction of entire training period over which the exploration rate is
         # reduced.
-        exploration_fraction: float = 0.1
+        exploration_fraction: float = uniform(0.05, 0.3, default=0.1)
         # Initial value of random action probability.
-        exploration_initial_eps: float = 1.0
+        exploration_initial_eps: float = uniform(0.5, 1.0, default=1.0)
         # final value of random action probability.
-        exploration_final_eps: float = 0.05
+        exploration_final_eps: float = uniform(0, 0.1, default=0.05)
         # The maximum value for the gradient clipping.
-        max_grad_norm: float = 10
+        max_grad_norm: float = uniform(1, 100, default=10)
         # Whether to create a second environment that will be used for
         # evaluating the agent periodically. (Only available when passing string
         # for the environment)
@@ -75,59 +76,6 @@ class DQNModel(DQN):
         # Whether or not to build the network at the creation
         # of the instance
         _init_setup_model: bool = True
-
-    def __init__(self,
-                 policy,
-                 env,
-                 learning_rate=0.0001,
-                 buffer_size=1000000,
-                 learning_starts=50000,
-                 batch_size=32,
-                 tau=1.0,
-                 gamma=0.99,
-                 train_freq=4,
-                 gradient_steps=1,
-                 n_episodes_rollout=-1,
-                 optimize_memory_usage=False,
-                 target_update_interval=10000,
-                 exploration_fraction=0.1,
-                 exploration_initial_eps=1.0,
-                 exploration_final_eps=0.05,
-                 max_grad_norm=10,
-                 tensorboard_log=None,
-                 create_eval_env=False,
-                 policy_kwargs=None,
-                 verbose=0,
-                 seed=None,
-                 device='auto',
-                 _init_setup_model=True,
-                ):
-        super().__init__(
-            policy,
-            env,
-            learning_rate=learning_rate,
-            buffer_size=buffer_size,
-            learning_starts=learning_starts,
-            batch_size=batch_size,
-            tau=tau,
-            gamma=gamma,
-            train_freq=train_freq,
-            gradient_steps=gradient_steps,
-            n_episodes_rollout=n_episodes_rollout,
-            optimize_memory_usage=optimize_memory_usage,
-            target_update_interval=target_update_interval,
-            exploration_fraction=exploration_fraction,
-            exploration_initial_eps=exploration_initial_eps,
-            exploration_final_eps=exploration_final_eps,
-            max_grad_norm=max_grad_norm,
-            tensorboard_log=tensorboard_log,
-            create_eval_env=create_eval_env,
-            policy_kwargs=policy_kwargs,
-            verbose=verbose,
-            seed=seed,
-            device=device,
-            _init_setup_model=_init_setup_model,
-        )
 
 @register_method
 @dataclass
@@ -175,18 +123,46 @@ class DQNMethod(StableBaselines3Method):
                 f"entries."
             ))
 
-            self.hparams.buffer_size = max_buffer_length
+            self.hparams.buffer_size = int(max_buffer_length)
 
         # Don't use up too many of the observations from the task to fill up the buffer.
         # Truth is, we should probably get this to work first.
-        if not setting.known_task_boundaries_at_train_time:
-            if self.hparams.buffer_size > setting.steps_per_task // 10:
+        
+        # NOTE: Need to change some attributes depending on the maximal number of steps
+        # in the environment allowed in the given Setting.
+        if setting.max_steps:
+            logger.info(
+                f"Total training steps are limited to {setting.steps_per_task} steps "
+                f"per task, {setting.max_steps} steps in total."
+            )
+            ten_percent_of_step_budget = setting.steps_per_task // 10
+            
+            if self.hparams.buffer_size > ten_percent_of_step_budget:
                 warnings.warn(RuntimeWarning(
-                    "Operating on a ContinualRL setting, so we're limiting the "
-                    "buffer size to 1/10 the number of steps in the training "
-                    "loop."
+                    "Reducing max buffer size to ten percent of the step budget."
                 ))
-                self.hparams.buffer_size = setting.steps_per_task // 10
+                self.hparams.buffer_size = ten_percent_of_step_budget
+
+            if self.hparams.learning_starts > ten_percent_of_step_budget:
+                logger.info(
+                    f"The model was originally going to use the first "
+                    f"{self.hparams.learning_starts} steps for pure random "
+                    f"exploration, but the setting has a max number of steps set to "
+                    f"{setting.max_steps}, therefore we will limit the number of "
+                    f"exploration steps to 10% of that 'step budget' = "
+                    f"{ten_percent_of_step_budget} steps."
+                )
+                self.hparams.learning_starts = ten_percent_of_step_budget
+
+            if self.hparams.target_update_interval > ten_percent_of_step_budget:
+                # Same for the 'update target network' interval.
+                self.hparams.target_update_interval = ten_percent_of_step_budget // 2
+                logger.info(
+                    f"Reducing the target network update interval to "
+                    f"{self.hparams.target_update_interval}, because of the limit on "
+                    f"training steps imposed by the Setting."
+                )
+
         logger.info(f"Will use a Replay buffer of size {self.hparams.buffer_size}.")
 
     def create_model(self, train_env: gym.Env, valid_env: gym.Env) -> DQNModel:
