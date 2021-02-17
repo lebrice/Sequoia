bfa3a6e0f07889a32bcefec3cfb2a8479bd26b0c
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-02-17 13:11:41 -0500

    Fix import bug in tests
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- tests/examples/quick_demo_test.py
+++ tests/examples/quick_demo_test.py
@@ -1,13 +1,12 @@
 """ TODO: Write tests that check that the examples are working correctly.
 """
-import pytest
 import contextlib
-# @pytest.fixture
-# def capture_results_summary(monkeypatch):
-    # 
 import sys
-from examples.basic.quick_demo import demo_command_line, demo_simple, ClassIncrementalSetting
-from sequoia.settings import Results
+
+import pytest
+from sequoia.settings import ClassIncrementalSetting, Results
+
+from examples.basic.quick_demo import demo_command_line, demo_simple
 
 
 @pytest.mark.timeout(120)
@@ -17,18 +16,18 @@ def test_quick_demo(monkeypatch):
     """
     results: ClassIncrementalSetting.Results = None
     summary_method = ClassIncrementalSetting.Results.summary
-    
+
     def summary(self: ClassIncrementalSetting.Results):
         nonlocal results
         results = self
         return summary_method(self)
-    
+
     monkeypatch.setattr(ClassIncrementalSetting.Results, "summary", summary)
 
     demo_simple()
-    
+
     from sequoia.common.metrics import ClassificationMetrics
-    
+
     # NOTE: Results aren't going to give *exactly* the same results, so we can't
     # test like this directly:
     # assert results.average_metrics_per_task == [
@@ -38,13 +37,13 @@ def test_quick_demo(monkeypatch):
     #     ClassificationMetrics(n_samples=2016, accuracy=0.835317),
     #     ClassificationMetrics(n_samples=1984, accuracy=0.99748),
     # ]
-    
+
     assert results.average_metrics_per_task[0].n_samples == 1984
     assert results.average_metrics_per_task[1].n_samples == 2016
     assert results.average_metrics_per_task[2].n_samples == 1984
     assert results.average_metrics_per_task[3].n_samples == 2016
     assert results.average_metrics_per_task[4].n_samples == 1984
-    
+
     assert 0.48 <= results.average_metrics_per_task[0].accuracy <= 0.55
     assert 0.48 <= results.average_metrics_per_task[1].accuracy <= 0.60
     assert 0.60 <= results.average_metrics_per_task[2].accuracy <= 0.95
