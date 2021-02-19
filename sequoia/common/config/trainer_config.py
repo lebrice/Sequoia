0dfbf276ec6146bd3c3de209df0d17b7c1ec3248
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-02-19 14:22:11 -0500

    Add max_epochs as a tunable hyper-parameter
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- sequoia/common/config/trainer_config.py
+++ sequoia/common/config/trainer_config.py
@@ -13,13 +13,14 @@ from pytorch_lightning.loggers import LightningLoggerBase
 from simple_parsing import choice, field, mutable_field
 from sequoia.utils.serialization import Serializable
 from sequoia.utils.parseable import Parseable
+from sequoia.common.hparams import HyperParameters, uniform
 from .config import Config
 
 from .wandb_config import WandbLoggerConfig
 
 
 @dataclass
-class TrainerConfig(Serializable, Parseable):
+class TrainerConfig(HyperParameters):
     """ Configuration options for the pytorch-lightning Trainer.
     
     TODO: Pytorch Lightning already has a mechanism for adding argparse
@@ -29,21 +30,22 @@ class TrainerConfig(Serializable, Parseable):
     gpus: int = torch.cuda.device_count()
     overfit_batches: float = 0.
     fast_dev_run: bool = field(default=False, nargs=0, action="store_true")
+
     # Maximum number of epochs to train for.
-    max_epochs: int = 10
+    max_epochs: int = uniform(1, 100, default=10)
+
     # Number of nodes to use.
     num_nodes: int = 1
     distributed_backend: Optional[str] = "dp" if gpus != 0 else None
     log_gpu_memory: bool = False
+    
     val_check_interval: Union[int, float] = 1.0
+    
     auto_scale_batch_size: Optional[str] = None
     auto_lr_find: bool = False
     # Floating point precision to use in the model. (See pl.Trainer)
     precision: int = choice(16, 32, default=32)
     default_root_dir: Path = Path(os.getcwd()) / "results"
-
-    # Wether to do the backward pass manually or automatically.
-    automatic_optimization: bool = True
 
     # How much of training dataset to check (floats = percent, int = num_batches)
     limit_train_batches: Union[int, float] = 1.0
@@ -96,7 +98,6 @@ class TrainerConfig(Serializable, Parseable):
             fast_dev_run=self.fast_dev_run,
             auto_scale_batch_size=self.auto_scale_batch_size,
             auto_lr_find=self.auto_lr_find,
-            automatic_optimization=self.automatic_optimization,
             # TODO: Either move the log-dir-related stuff from Config to this
             # class, or figure out a way to pass the value from Config to this
             # function
