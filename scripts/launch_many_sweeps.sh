902bcaf82761a6efbd4a997e8c7752a8059131bc
Oleksiy Ostapenko <oleks.ostapenko@icloud.com>
2021-02-25 16:08:13 -0500

    scripts

--- scripts/launch_many_sweeps.sh
+++ scripts/launch_many_sweeps.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-module load anaconda/3
-conda activate sequoia
+# module load anaconda/3
+# conda activate sequoia
 
 cd ~/Sequoia
-pip install -e .[hpo,monsterkong]
+# pip install -e .[hpo,monsterkong]
+
 
 settings="incremental_rl task_incremental_rl rl"
 nb_tasks="10 20"
@@ -14,5 +15,5 @@ for nb_task in $nb_tasks ;do
 for method in $methods ;do
 sbatch scripts/sweep.sh --setting $setting --dataset monsterkong --steps_per_task 100_000 --nb_tasks $nb_task --method $method
 done #methods
-done #nb_tasks
+done #nb_tasks 
 done #settings
--- scripts/sweep.sh
+++ scripts/sweep.sh
@@ -4,12 +4,19 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=10GB
 #SBATCH --time=11:59:00
+#SBATCH -p long
+#SBATCH -o logs/slurm-%j.out
 
-module load anaconda/3
-conda activate sequoia
+# module load anaconda/3
+# conda activate sequoia
 
-cd ~/Sequoia
+cd ~/dev/Sequoia
+
+module purge  
+module load python/3.7    
+module load python/3.7/cuda/11.0/cudnn/8.0/pytorch/1.6.0
+source /home/mila/o/ostapeno/ENV/SSCL/bin/activate
+
 pip install -e .[hpo,monsterkong]
-
 # TODO: Change the setting, the number of tasks, the method, etc.
-sequoia_sweep --data_dir $SLURM_TMPDIR "$@"
+sequoia_sweep "$@"
