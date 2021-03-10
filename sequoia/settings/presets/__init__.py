2b2f8debb9c9b9649f3b295caa69b92b606fcded
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-03-10 18:37:22 -0500

    Add the sl track as a preset
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- sequoia/settings/presets/__init__.py
+++ sequoia/settings/presets/__init__.py
@@ -4,11 +4,6 @@ from pathlib import Path
 presets_dir = Path(os.path.dirname(__file__))
 
 setting_presets = {
-    "cartpole_state": presets_dir / "cartpole_state.yaml",
-    "cartpole_pixels": presets_dir / "cartpole_pixels.yaml",
-    "mnist": presets_dir / "mnist.yaml",
-    "fashion_mnist": presets_dir / "fashion_mnist.yaml",
-    "cifar10": presets_dir / "cifar10.yaml",
-    "cifar100": presets_dir / "cifar100.yaml",
-    "monsterkong": presets_dir / "monsterkong_pixels.yaml",
+    file.stem: file    
+    for file in presets_dir.glob("*.yaml")
 }
