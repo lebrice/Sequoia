8a191de59a017c213bc900229afa4b107b0e28b1
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-04-17 00:00:52 -0400

    Fix contains() of TensorBox space
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- sequoia/common/spaces/tensor_box.py
+++ sequoia/common/spaces/tensor_box.py
@@ -31,32 +31,49 @@ class TensorBox(spaces.Box):
     def __init__(
         self, low, high, shape=None, dtype=np.float32, device: torch.device = None
     ):
-        if dtype in numpy_to_torch_dtypes:
+        if any(dtype == key for key in numpy_to_torch_dtypes):
             self._numpy_dtype = dtype
-            self._torch_dtype = numpy_to_torch_dtypes[dtype]
-        elif dtype in torch_to_numpy_dtypes:
-            self._numpy_dtype = torch_to_numpy_dtypes[dtype]
+            self._torch_dtype = [
+                v for k, v in numpy_to_torch_dtypes.items() if k == dtype
+            ][0]
+        elif any(dtype == key for key in torch_to_numpy_dtypes):
+            self._numpy_dtype = [
+                v for k, v in torch_to_numpy_dtypes.items() if k == dtype
+            ][0]
             self._torch_dtype = dtype
         elif str(dtype) == "float32":
             self._numpy_dtype = np.float32
             self._torch_dtype = torch.float32
         else:
+            assert not any(dtype == k for k in numpy_to_torch_dtypes.keys())
+            assert not any(dtype == k for k in torch_to_numpy_dtypes.keys())
             raise NotImplementedError(
                 f"Unsupported dtype {dtype} (of type {type(dtype)})"
             )
         super().__init__(low, high, shape=shape, dtype=self._numpy_dtype)
         self.device: Optional[torch.device] = torch.device(device) if device else None
+        self.low_tensor = torch.as_tensor(self.low, device=self.device)
+        self.high_tensor = torch.as_tensor(self.high, device=self.device)
 
     def sample(self):
         sample = super().sample()
-        return torch.as_tensor(sample, dtype=self._torch_dtype, device=self.device)
+        return torch.as_tensor(sample, device=self.device)
 
     def contains(self, x: Union[list, np.ndarray, Tensor]) -> bool:
         if isinstance(x, list):
             x = np.array(x)  # Promote list to array for contains check
+        if isinstance(x, Tensor):
+            return (
+                x.shape == self.shape
+                and (x >= self.low_tensor).all()
+                and (x <= self.high_tensor).all()
+            )
         return (
             x.shape == self.shape and np.all(x >= self.low) and np.all(x <= self.high)
         )
 
     def __repr__(self):
-        return f"{type(self).__name__}({self.low.min()}, {self.high.max()}, {self.shape}, {self.dtype}, device={self.device})"
+        return (
+            f"{type(self).__name__}({self.low.min()}, {self.high.max()}, "
+            f"{self.shape}, {self.dtype}, device={self.device})"
+        )
