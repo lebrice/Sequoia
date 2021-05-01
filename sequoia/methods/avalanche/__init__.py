266f859804d1c65a6df9a623ebe3e94694eeb785
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-05-01 18:45:10 +0000

    Add missing import for gdumb method
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- sequoia/methods/avalanche/__init__.py
+++ sequoia/methods/avalanche/__init__.py
@@ -4,6 +4,8 @@ from .ar1 import AR1Method
 from .cwr_star import CWRStarMethod
 from .ewc import EWCMethod
 from .gem import GEMMethod
+# Still quite buggy, needs to be fixed on the avalanche side.
+from .gdumb import GDumbMethod
 from .lwf import LwFMethod
 from .naive import NaiveMethod
 from .replay import ReplayMethod
