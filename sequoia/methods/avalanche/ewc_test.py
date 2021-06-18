c59bddcd5b338d73cda12a87d28423ab1dc183f4
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-06-18 18:20:48 -0400

    Fix bugs in tests for avalanche.EWC
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- sequoia/methods/avalanche/ewc_test.py
+++ sequoia/methods/avalanche/ewc_test.py
@@ -23,13 +23,7 @@ from .patched_models import MTSimpleCNN, MTSimpleMLP, SimpleCNN, SimpleMLP
 
 class TestEWCMethod(_TestAvalancheMethod):
     Method: ClassVar[Type[AvalancheMethod]] = EWCMethod
-    ignored_parameter_differences: ClassVar[List[str]] = [
-        "device",
-        "eval_mb_size",
-        "criterion",
-        "train_mb_size",
-        "train_epochs",
-        "evaluator",
+    ignored_parameter_differences: ClassVar[List[str]] = _TestAvalancheMethod.ignored_parameter_differences + [
         "decay_factor",
     ]
 
