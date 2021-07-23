784faff7e8dab9ba4c296051ac362d0e2b36a967
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-07-23 12:14:59 -0400

    Fix ContinualSL Results not being used correctly
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- sequoia/settings/assumptions/continual.py
+++ sequoia/settings/assumptions/continual.py
@@ -95,11 +95,10 @@ class ContinualAssumption(AssumptionBase):
     @dataclass(frozen=True)
     class Actions(AssumptionBase.Actions):
         pass
-    
+
     @dataclass(frozen=True)
     class Rewards(AssumptionBase.Rewards):
         pass
-
 
     # TODO: Move everything necessary to get ContinualRLSetting to work out of
     # Incremental and into this here. Makes no sense that ContinualRLSetting inherits
@@ -127,16 +126,20 @@ class ContinualAssumption(AssumptionBase):
         logger.info(f"Starting training")
         method.set_training()
         self._start_time = time.process_time()
-        
+
         method.fit(
             train_env=train_env, valid_env=valid_env,
         )
         train_env.close()
         valid_env.close()
 
-
         logger.info(f"Finished Training.")
-        results: ContinualResults = self.test_loop(method)
+
+        results = self.test_loop(method)
+        if isinstance(results, TaskResults):
+            results = ContinualResults(metrics=results.metrics)
+
+        assert isinstance(results, ContinualResults)
 
         if self.monitor_training_performance:
             results._online_training_performance = train_env.get_online_performance()
