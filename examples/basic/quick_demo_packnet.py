389426fea73813386c9275c814b701713bef9950
Lucas Cecchi <lucascecchi@gmail.com>
2021-08-12 13:50:07 -0400

    Small import change

--- examples/basic/quick_demo_packnet.py
+++ examples/basic/quick_demo_packnet.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
-from sequoia.settings.sl import TaskIncrementalSLSetting
+from sequoia.settings.sl import TaskIncrementalSLSetting, ContinualSLSetting
 from sequoia.settings.sl.continual import Observations, Rewards, Environment
 from sequoia.methods.packnet_method import PackNetMethod
 from examples.basic.pl_example import Model
