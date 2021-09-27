Fix test evaluation - use average of agent rewards to get single scalar reward in multi-agent setting

sequoia/methods/marl_poc_rl.py
@@ -22,6 +22,7 @@ except ImportError:
     from typing_extensions import Literal
 
 
+# This creates a vectorized environment that has multiple pistonball environments concatenated together
 def make_pistonball_env(
         vector_env_type: Literal["gym", "stable_baselines3", "stable_baselines"] = "stable_baselines3"
 ) -> VectorEnv:
@@ -48,6 +49,36 @@ def make_pistonball_env(
         env.reward_range = (-np.inf, np.inf)
         env.single_action_space = env.venv.action_space
     return env
+
+# TODO: Condense this into one method with above method that will take num_vec_envs as a parameter
+# For testing, this creates a single pistonball environment (vectorized across agents)
+def make_test_pistonball_env(
+        vector_env_type: Literal["gym", "stable_baselines3", "stable_baselines"] = "stable_baselines3"
+) -> VectorEnv:
+    env = pistonball_v4.parallel_env(
+        n_pistons=20,
+        local_ratio=0,
+        time_penalty=-0.1,
+        continuous=True,
+        random_drop=True,
+        random_rotate=True,
+        ball_mass=0.75,
+        ball_friction=0.3,
+        ball_elasticity=1.5,
+        max_cycles=125,
+    )
+    env = ss.color_reduction_v0(env, mode="B")
+    env = ss.resize_v0(env, x_size=84, y_size=84)
+    env = ss.frame_stack_v1(env, 3)
+    vector_env: VectorEnv = ss.pettingzoo_env_to_vec_env_v0(env)
+    env = ss.concat_vec_envs_v0(vector_env, 1, num_cpus=4, base_class=vector_env_type)
+    env.spec = EnvSpec("Sequoia_PistonBall-v4", entry_point=make_test_pistonball_env)
+    if vector_env_type == "stable_baselines3":
+        # Patch missing attributes
+        env.reward_range = (-np.inf, np.inf)
+        env.single_action_space = env.venv.action_space
+    return env
+
 
 
 gym.register("Sequoia_PistonBall-v4", entry_point=make_pistonball_env)
@@ -101,7 +132,8 @@ from sequoia.settings.rl import TaskIncrementalRLSetting
 def main():
     pistonball_env = make_pistonball_env()  # gym.make("Sequoia_PistonBall-v4")
     pistonball_val_env = make_pistonball_env()  # gym.make("Sequoia_PistonBall-v4")
-    pistonball_test_env = make_pistonball_env()  # gym.make("Sequoia_PistonBall-v4")
+    # pistonball_test_env = make_pistonball_env()  # gym.make("Sequoia_PistonBall-v4")
+    pistonball_test_env = make_test_pistonball_env()  # gym.make("Sequoia_PistonBall-v4")
 
     model = PPO(
         CnnPolicy,

sequoia/settings/assumptions/continual.py
@@ -1,6 +1,7 @@
 import itertools
 import json
 import time
+import numpy as np
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from io import StringIO
@@ -423,11 +424,12 @@ class TestEnvironment(gym.wrappers.Monitor, IterableWrapper[EnvType], ABC):
         else:
             done = bool(done)
 
-        # TODO: Do i have to sum all the agents' reward together to get one scalar reward for logging?
-        # TODO: Have 20 pistons per game, and 8 envs parallelized - so reward_for_stats is size-160 numpy array
-        # For now, just going to take first reward, but this isn't correct
-        # done = self._after_step(observation_for_stats, reward_for_stats, done, info)
-        done = self._after_step(observation_for_stats, reward_for_stats[0], done, info)
+        # If the environment is a multi-agent environment, the reward_for_stats will be a vector of agent rewards,
+        # which must be condensed to a single scalar reward
+        if reward_for_stats.size > 1:
+            # Have n agents per game (i.e. 20 pistons) in 1 vectorized environment
+            # To get reward to evaluate on, we take the average of rewards of all n agents
+            done = self._after_step(observation_for_stats, np.mean(reward_for_stats), done, info)
 
         if self.get_total_steps() >= self.step_limit:
             done = True