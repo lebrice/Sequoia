b0720af3924ba1ef2df1b0a35570b4d0754bbd89
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-09-28 19:54:51 -0400

    Allow passing custom registry
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- scripts/eai/job.sh
+++ scripts/eai/job.sh
@@ -10,7 +10,8 @@ ORG_NAME=$(eai organization get --field name)
 ACCOUNT_NAME=$(eai account get --field name)
 ACCOUNT_ID=$ORG_NAME.$ACCOUNT_NAME
 
-EAI_Registry=registry.console.elementai.com/$ACCOUNT_ID
+EAI_Registry=${EAI_Registry:-"registry.console.elementai.com/$ACCOUNT_ID"}
+echo "Using registry $EAI_Registry"
 
 CURRENT_BRANCH="`git branch --show-current`"
 BRANCH=${BRANCH:-$CURRENT_BRANCH}
