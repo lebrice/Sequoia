187f992ab6657cd09cf1504bfeccbf14cd7a8ada
Lucas Cecchi <lucascecchi@gmail.com>
2021-10-01 10:51:08 -0400

    Switched to DQNMethod instead of SAC. First functioning version of OfflineRL Setting

--- sequoia/settings/offline_rl/setting.py
+++ sequoia/settings/offline_rl/setting.py
@@ -9,7 +9,7 @@ from d3rlpy.dataset import MDPDataset
 from torch.utils.data import random_split
 from gym.wrappers import Monitor
 from gym.wrappers.record_episode_statistics import RecordEpisodeStatistics
-from d3rlpy.algos import SAC
+from d3rlpy.algos import SAC, DQN
 from simple_parsing.helpers import choice
 from torch.utils.data import DataLoader
 from sequoia.settings.base import Method
@@ -19,6 +19,7 @@ import gym
 from dataclasses import dataclass
 from d3rlpy.metrics.scorer import td_error_scorer
 from d3rlpy.metrics.scorer import average_value_estimation_scorer
+from sequoia.common.gym_wrappers.transform_wrappers import TransformObservation, TransformReward
 
 
 @dataclass
@@ -26,16 +27,9 @@ class OfflineRLSetting(Setting):
     # available_datasets: ClassVar[List[str]] = ["CartPole-v0"]
     # dataset: str = choice(available_datasets)
     dataset: str = "CartPole-v0"
-    val_size: int = 0.1
+    val_size: int = 0.2
     test_steps: int = 10_000
     seed: int = 123
-
-    def __post_init__(self):
-        torch.random.manual_seed(self.seed)
-        self.train_dataset, self.valid_dataset = train_test_split(self.dataset, test_size=self.val_size)
-
-    def prepare_data(self, dataset):
-        self.dataset, self.env = d3rlpy.datasets.get_cartpole(self.dataset)
 
     def train_dataloader(self, batch_size: int = None) -> DataLoader:
         return DataLoader(self.train_dataset, batch_size=batch_size)
@@ -46,44 +40,45 @@ class OfflineRLSetting(Setting):
     def apply(self, method: Method["OfflineRLSetting"]) -> Results:
         method.configure(self)
 
-        method.fit(train_env=self.train_dataset, valid_env=self.valid_dataset)
+        if self.dataset == "CartPole-v0":
+            self.dataset, self.env = d3rlpy.datasets.get_cartpole()
+        else:
+            self.dataset, self.env = d3rlpy.datasets.get_dataset(self.dataset)
 
-        self.env.seed(self.seed)
-        self.env = Monitor(self.env, directory="/results")
+        self.train_dataset, self.valid_dataset = train_test_split(self.dataset, test_size=self.val_size)
+        method.fit(train_env=self.train_dataset,
+                   valid_env=self.valid_dataset)
 
-
-        while steps < self.test_steps and self.env.is_closed():
-            obs = self.env.reset()
-            done = False
-            while not done:
-                action = method.get_actions(obs, action_space=self.action_space)
-                obs, reward, done, info = self.env.step()
-        # todo: get the statistics from the env.
-        stats = self.env.get_results()
-        result = self.Results(stats)
-        return result
+        #
+        # Evaluate on environment
+        # while env not done ...
+        #
 
 
-class SACMethod(Method, target_setting=OfflineRLSetting):
-    def __init__(self, train_steps: int = 1_000_000, n_epochs: int = 100, scorers: dict = None):
+class DQNMethod(Method, target_setting=OfflineRLSetting):
+    def __init__(self, train_steps: int = 1_000_000, n_epochs: int = 5, scorers: dict = None):
         super().__init__()
         self.train_steps = train_steps
         self.n_epochs = n_epochs
         self.scorers = scorers
-        self.algo: SAC
+        self.algo: DQN
 
     def configure(self, setting: OfflineRLSetting):
         super().configure(setting)
         # prepare algorithm
-        self.algo = SAC()
+        self.algo = DQN()
 
     def fit(self, train_env: MDPDataset, valid_env: MDPDataset) -> None:
         # train offline
-        self.algo.fit(train_env,
-                      eval_episodes=valid_env,
-                      n_steps=self.train_steps,
-                      n_epochs=self.n_epochs,
-                      scorers=self.scorers)
+        if isinstance(train_env, MDPDataset):
+            self.algo.fit(train_env,
+                          eval_episodes=valid_env,
+                          n_epochs=self.n_epochs,
+                          scorers=self.scorers)
+        else:
+            pass
+            # train_env = d3rlpy_wrapper(train_env)
+            # self.algo.fit_online(train_env)
 
     def get_actions(self, obs: np.ndarray, action_space: Space) -> np.ndarray:
         # ready to control
@@ -92,7 +87,7 @@ class SACMethod(Method, target_setting=OfflineRLSetting):
 
 def main():
     setting = OfflineRLSetting(dataset="CartPole-v0")
-    method = SACMethod(scorers={
+    method = DQNMethod(scorers={
             'td_error': td_error_scorer,
             'value_scale': average_value_estimation_scorer
         })
