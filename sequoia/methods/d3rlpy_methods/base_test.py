43ffd8675fb8cd7c8f08b502e331bca5cf6a3f53
Lucas Cecchi <lucascecchi@gmail.com>
2021-11-20 20:34:53 -0500

    Fix import err

--- sequoia/methods/d3rlpy_methods/base_test.py
+++ sequoia/methods/d3rlpy_methods/base_test.py
@@ -1,5 +1,7 @@
 import pytest
 from d3rlpy.constants import ActionSpace
+
+from sequoia import TraditionalRLSetting
 from sequoia.methods.d3rlpy_methods.base import *
 from sequoia.settings.offline_rl.setting import OfflineRLSetting
 
