17b2158f60cb22468faa73211ff3e8b7ce8e55df
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-02-08 19:11:38 -0500

    Remove unused Auxiliary Tasks
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- sequoia/methods/aux_tasks/__init__.py
+++ sequoia/methods/aux_tasks/__init__.py
@@ -1,10 +1,7 @@
 from .auxiliary_task import AuxiliaryTask
 from .ewc import EWCTask
-from .irm import IrmTask
-from .jigsaw_puzzle import JigsawPuzzleTask
-from .mixup import ManifoldMixupTask, MixupTask
 from .reconstruction import AEReconstructionTask, VAEReconstructionTask
-from .transformation_based import AdjustBrightnessTask, RotationTask
+from .transformation_based import RotationTask
 
 VAE: str = VAEReconstructionTask.name
 AE: str = AEReconstructionTask.name
--- sequoia/methods/aux_tasks/transformation_based/__init__.py
+++ sequoia/methods/aux_tasks/transformation_based/__init__.py
@@ -1,11 +1,2 @@
-from .adjust_brightness import AdjustBrightnessTask
 from .bases import ClassifyTransformationTask, RegressTransformationTask, TransformationBasedTask
 from .rotation import RotationTask
-
-__all__ = [
-    "AdjustBrightnessTask",
-    "ClassifyTransformationTask",
-    "RegressTransformationTask",
-    "TransformationTask",
-    "RotationTask",
-]
